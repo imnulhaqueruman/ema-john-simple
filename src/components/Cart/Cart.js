804891b00a4a7b9833d252e03c044b308261d2f2
Imnul <imnulhaqueruman18@gmail.com>
2021-02-28 01:41:59 +0600

    finalupdate

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    console.log(cart);
+    let total = 0;
+    for(let i = 0; i<cart.length; i++){
+        const product = cart[i];
+        total = total + product.price;
+    }
+    let shipping = 0;
+    if(total>35){
+        shipping = 0;
+    }
+    else if(total>15){
+        shipping = 4.99;
+    }
+    else if(total>0){
+        shipping = 12.99;
+    }
+    const tax = (total * 0.1);
+    const formatNumber = (num) =>{
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+    return (
+        <div className="cart">
+            <h2>Order summary</h2>
+            <h3>Items ordered:{cart.length}</h3>
+            <p><small>Product Price:{formatNumber(total)}</small></p>
+            <p><small>Shipping:{shipping}</small></p>
+            <p><small>Tax:{formatNumber(tax)}</small></p>
+            <p>Total:{formatNumber(total + shipping + tax)}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Header/Product/Product.js
+++ src/components/Header/Product/Product.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product.name);
+    console.log(props);
     const {img,name,seller,price,stock} = props.product;
     return (
         <div className='product'>
@@ -17,6 +19,9 @@ const Product = (props) => {
                <p>${price}</p>
                <br/>
                <p><small>Only{stock} left in stock -order soon</small></p>
+               <button className="main-button"
+               onClick={() =>props.handleAddProduct(props.product)}
+               > <FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
             </div>
            
            
